ec4592836323b32043e0818aa779ad3510f9fb5b
TheMaster0fFire <seth.development@gmail.com>
2016-05-11 13:59:47 -0700

    michael please fix crashes

--- apcsasphero/src/main/java/net/gravityfox/apcsa/sphero/apcsasphero/apcsasphero/MainActivity.java
+++ apcsasphero/src/main/java/net/gravityfox/apcsa/sphero/apcsasphero/apcsasphero/MainActivity.java
@@ -1,11 +1,15 @@
 package net.gravityfox.apcsa.sphero.apcsasphero.apcsasphero;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "net.gravityfox.apcsa.sphero.apcsasphero.apcsasphero.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,5 +37,16 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        setContentView(R.layout.activity_main);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        if (editText != null) {
+            String message = editText.getText().toString();
+            intent.putExtra(EXTRA_MESSAGE, message);
+            startActivity(intent);
+        } else System.out.println("Error!");
     }
 }
