2c1a945e863e698e770939e1e21a23203f963590
TheMaster0fFire <seth.development@gmail.com>
2016-05-26 13:18:57 -0700

    ball slow

--- apcsasphero/src/main/java/net/gravityfox/apcsa/sphero/apcsasphero/apcsasphero/MainActivity.java
+++ apcsasphero/src/main/java/net/gravityfox/apcsa/sphero/apcsasphero/apcsasphero/MainActivity.java
@@ -1,77 +1,69 @@
 package net.gravityfox.apcsa.sphero.apcsasphero.apcsasphero;
 
-import android.content.Intent;
-import android.content.pm.ActivityInfo;
-import android.hardware.SensorManager;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.EditText;
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
-import android.hardware.SensorEvent;
-import android.widget.TextView;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener {
-    private TextView xText, yText, zText;
-    private Sensor mySensor;
-    private SensorManager SM;
+public class MainActivity extends Activity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-
-        //Create our sensor manager
-        SM = (SensorManager)getSystemService(SENSOR_SERVICE);
-
-        //Accelerometer sensor
-        mySensor = SM.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-
-        //Register sensor
-        SM.registerListener((SensorEventListener) this, mySensor, SensorManager.SENSOR_DELAY_GAME);
-
-        xText = (TextView)findViewById(R.id.xText);
-        yText = (TextView)findViewById(R.id.yText);
-        zText = (TextView)findViewById(R.id.zText);
+        setContentView(new MyView(this));
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    public class MyView extends View implements SensorEventListener{
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        private SensorManager SM;
+        private Sensor sensor;
+        private float x = getWidth()/2;
+        private float y = getHeight()/2;
+        private float lastx = 0;
+        private float lasty = 0;
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        public MyView(Context context) {
+            super(context);
+            SM = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+            sensor = SM.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+            SM.registerListener(this, sensor , SensorManager.SENSOR_DELAY_GAME);
         }
 
-        return super.onOptionsItemSelected(item);
-    }
+        @Override
+        protected void onDraw(Canvas canvas) {
+            // TODO Auto-generated method stub
+            super.onDraw(canvas);
+            int radius = 70;
+            Paint paint = new Paint();
+            paint.setStyle(Paint.Style.FILL);
+            paint.setColor(Color.WHITE);
+            canvas.drawPaint(paint);
+            // Use Color.parseColor to define HTML colors
+            paint.setColor(Color.parseColor("#CD5C5C"));
+            canvas.drawCircle(lastx + ((-1*x)/2), lasty + ((-1*y)/2), radius, paint);
+        }
 
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-        xText.setText("X: " + event.values[0]);
-        yText.setText("Y: " + event.values[1]);
-        zText.setText("Z: " + event.values[2]);
-    }
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            //lastx = x;
+            //lasty = y;
+            //x = event.values[0];
+            //y = event.values[1];
+            x--;
+            y--;
+            postInvalidate();
+        }
 
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-        //not in use
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            //not used
+        }
     }
 }
